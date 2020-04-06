e5c7c2b6da58d2dfdafd49169b24293669abb722
leonardo sm <leomallmann020302@gmail.com>
2020-04-05 23:11:45 -0300

    register repaging

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,68 +1,67 @@
 import React, {useState} from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
 
 import './styles.css';
-import './stylesAside.css';
 import './stylesTopBar.css';
+
+import ifLogo from '../../images/ifLogo.png';
 
 export default function Register(){
     
     return(
 <div className="menu">
      
-        <li><Link className="back-link" to="/AboutTheTeam">Conheça o time</Link></li>
-        <li> <Link className="back-link" to="/">Conheça o time</Link> </li>
-        <li> <Link className="back-link" to="/">Conheça o time</Link> </li>
-        <li> <Link className="back-link" to="/">Conheça o time</Link> </li> 
+        <li> <Link className="topBar-link" to="/AboutTheTeam">Conheça o time</Link> </li>
+        <li> <Link className="topBar-link" to="/">Conheça o time</Link> </li>
+        <li> <Link className="topBar-link" to="/">Conheça o time</Link> </li>
+        <li> <Link className="topBar-link" to="/">Conheça o time</Link> </li> 
 
-<div className="aside_bar">
+        <div className="register-container">
+           <div className="content">
+                <section>
+                    <img src={ifLogo} width="420" alt="be the hero"/>
 
-    <aside>
-        <strong>Inscreva-se</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="nome">Nome</label>
-            <input name="nome" id="nome"/>
-          </div>
-          
-          <div className="input-block">
-            <label htmlFor="email">E-mail</label>
-            <input name="email" id="email"/>
-          </div>
+                    <h1>Inscreva-se</h1>
+                    <p>Faça sua inscrição e fique atento às datas de realização dos testes.</p>
 
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="telefone">Telefone</label>
-              <input type="text" name="telefone" id="telefone" maxLength="15"/>
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="longitude">Idade</label>
-              <input type="number" name="longitude" min="14" max="19" id="longitude"/>
-            </div>
-          </div>
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="E02041"/>
+                        Voltar para home
+                    </Link>
+                </section>
 
-          <button type="submit" on>Salvar</button>
-        </form>
-      </aside>
-      
-      <ul className="msg">
-      
-      <textbox className="tb_1">
-      <span > _vá além <br/>das salas de aula.</span> 
-      </textbox>
-      
-      <textbox className="tb_3">
-      <span> _faça parte da<br/> história.</span> 
-      </textbox>
-      
-      <textbox className="tb_4">
-      <span> _junte-se<br/>às nossas 
-      equipes.</span> 
-      </textbox>
-      </ul>
+                <form >
+                    <input placeholder="Nome do estudante"
+                    
+                    
+                    />
 
-    </div>
+                    <input type="email" placeholder="E-mail"
+                    
+                    />
+
+                    <input placeholder="Whatsapp"
+                    
+                    />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade"
+                       
+                        />
+
+                        <input placeholder="Data nasc." 
+                        style={{width: 122}}
+                        
+                        />
+                    </div>
+
+                    <button className="button" type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+           </div>
+       </div>
     </div>
     );
 }
