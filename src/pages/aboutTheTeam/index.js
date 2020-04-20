b6675a2644cc6e7ef5fcfd4442fe8f212eb7a8f3
leonardo sm <leomallmann020302@gmail.com>
2020-04-20 19:09:08 -0300

    Divulgando os espaços

--- src/pages/aboutTheTeam/index.js
+++ src/pages/aboutTheTeam/index.js
@@ -4,10 +4,13 @@ import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import '../../global.css'
 
+import ifsul_field from '../../images/ifsul_field.jpg';
+import ifsul_gym from '../../images/ifsul_gym.jpg';
+
 export default function AboutTeam(){
     
     return(
-       <div>
+       <div className="corpo">
          <header className="title">
            <img src="" alt="Volei Foto"/>
 
@@ -90,8 +93,37 @@ export default function AboutTeam(){
             
             </div>
 
+          </div>
 
-         </div>
+          
+          
+          <div className="galery">
+                <h1>Our <br/> Spaces</h1>
+
+          <div className="places">
+
+          <div className="field">
+            <img
+              src={ifsul_field}
+              width="600"
+              height="300"
+            />
+
+            <h1>Quadra Poliesportiva</h1>
+          </div>
+
+          <div className="gym">
+            <img
+              src={ifsul_gym}
+              width="600"
+              height="300"
+            />
+
+            <h1>Academia</h1>
+          </div>
+          </div>
+
+          </div>
        </div>
     );
 }
