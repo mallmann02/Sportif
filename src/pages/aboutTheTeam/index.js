4af11b95ab2c814e7b173bfa2a95c64d68a6de66
leonardo sm <leomallmann020302@gmail.com>
2020-04-20 23:34:54 -0300

    reformulando pagina equipe voleibol

--- src/pages/aboutTheTeam/index.js
+++ src/pages/aboutTheTeam/index.js
@@ -4,6 +4,7 @@ import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import '../../global.css'
 
+import voleiLogo from '../../images/voleiLogo.png';
 import ifsul_field from '../../images/ifsul_field.jpg';
 import ifsul_gym from '../../images/ifsul_gym.jpg';
 
@@ -12,85 +13,17 @@ export default function AboutTeam(){
     return(
        <div className="corpo">
          <header className="title">
-           <img src="" alt="Volei Foto"/>
+           <img src={voleiLogo} width="100" alt="Volei Foto"/>
 
            <h1>Vôlei Masculino</h1>
          </header>
 
          
          <div className="main">
-            <img src="" alt="uniforme"/>
 
-            
             <div className="msg">
-              <group>
-              <textbox>Novo uniforme de 2020</textbox>
-
-              <textbox>Vista o manto.</textbox>              
-              </group>
-
-
-              <container>
-                <header>
-                  <img src="" alt="Aluno"/>
-
-                  <h1>Virgílio Ramires</h1>
-                  <h2>Tutor de E.F e treinador do time de vôlei</h2>
-                </header>
-
-
-              </container>
-            </div>
-
-            <div className="players">
-              <group>
-              <container>
-                <header>
-                  <img src="" alt="Aluno"/>
-
-                  <h1>Aluno 1</h1>
-                  <h2>Descrição aluno 1</h2>
-                </header>
-
-
-              </container>
-
-              <container>
-                <header>
-                  <img src="" alt="Aluno"/>
-
-                  <h1>Aluno 1</h1>
-                  <h2>Descrição aluno 1</h2>
-                </header>
-
-
-              </container>
-              </group>
-
-              <group>
-              <container>
-                <header>
-                  <img src="" alt="Aluno"/>
-
-                  <h1>Aluno 1</h1>
-                  <h2>Descrição aluno 1</h2>
-                </header>
-
-
-              </container>
-
-              <container>
-                <header>
-                  <img src="" alt="Aluno"/>
-
-                  <h1>Aluno 1</h1>
-                  <h2>Descrição aluno 1</h2>
-                </header>
-
-
-              </container>
-              </group>
-            
+              
+             
             </div>
 
           </div>
@@ -124,6 +57,8 @@ export default function AboutTeam(){
           </div>
 
           </div>
+
+         
        </div>
     );
 }
